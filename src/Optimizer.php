69514fa91829075aed9d4ad10f1d92cd6705b32f
Danilo Monteiro <dmonteiro.souza@gmail.com>
2018-10-17 21:42:55 -0300

    add admins verification

--- src/Optimizer.php
+++ src/Optimizer.php
@@ -131,10 +131,12 @@ class Optimizer extends MetaTags
             return $this;
         }
 
-        foreach ($admins as $admin) {
-            $fb = $this->meta->addChild("meta");
-            $fb->addAttribute("property", "fb:admins");
-            $fb->addAttribute("content", $admin);
+        if($admins) {
+            foreach ($admins as $admin) {
+                $fb = $this->meta->addChild("meta");
+                $fb->addAttribute("property", "fb:admins");
+                $fb->addAttribute("content", $admin);
+            }
         }
 
         return $this;
